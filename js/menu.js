1652c9c333f7a7c819e8f858c639ce2cd3c8baae
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2022-12-05 03:42:11 +0000

    push

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 const menuOverlay = document.querySelector('.mobile-menu');
 const closeMenuButton = document.querySelector('.mobile-menu__vsg');
-const openMenuBtn = document.querySelector('.hamburger-menu__vsg');
+const openMenuBtn = document.querySelector('.header-nav__icon');
 
 openMenuBtn.addEventListener('click', e => {
     menuOverlay.classList.remove('d-none');
