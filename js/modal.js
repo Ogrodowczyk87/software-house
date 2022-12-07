d3c00c17f595e81ed83a58a23dba105c640ffb46
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2022-12-07 16:26:38 +0000

    push

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,7 @@
 (() => {
     const refs = {
         openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[backdrop__button]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
         modal: document.querySelector("[data-modal]"),
     };
 
